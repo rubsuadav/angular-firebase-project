1fc6ac7daaab198a1d432b16169138f9e5390f07
rubsuadav <rubsuadav@alum.us.es>
2024-11-28 18:00:20 +0100

    Add route guards for access pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { anonymousGuard } from './guards/anonymous.guard';
+import { authenticatedGuard } from './guards/authenticated.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [anonymousGuard],
+  },
+  { path: 'login', component: LoginComponent, canActivate: [anonymousGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [authenticatedGuard] },
 ];
