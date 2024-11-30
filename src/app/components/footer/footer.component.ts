bb302a99857b78fa9c29f261975fe102797c7bfc
rubsuadav <rubsuadav@alum.us.es>
2024-11-30 12:21:40 +0100

    Add navbar and footer components to the main layout

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {
+  protected year: number = new Date().getFullYear();
+}
